386fa5a8ffdd86a82241de5548b0b5c2c6422950
Genina Tano <tanogenina@gmail.com>
2025-05-16 23:08:52 +0800

    Teacher Quiz

---
+++ app/src/main/java/com/example/css/QT_TeacherQuiz.kt
@@ -0,0 +1,28 @@
+package com.example.css
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class QT_TeacherQuiz : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_qt_teacher_quiz)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val takeQuizButton = findViewById<ImageButton>(R.id.btn_take_quiz_1)
+        takeQuizButton.setOnClickListener {
+            val intent = Intent(this, QT_TeacherQuiz_MainGame::class.java)
+            startActivity(intent)
+        }
+    }
+}
