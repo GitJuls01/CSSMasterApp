98bdcf9f2e58146d11f7b84a997271c22095460f
Genina Tano <tanogenina@gmail.com>
2025-06-08 18:38:11 +0800

    Create Quiz UI

--- app/src/main/java/com/example/css/QT_TeacherQuiz.kt
+++ app/src/main/java/com/example/css/QT_TeacherQuiz.kt
@@ -10,7 +10,10 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.firestore.FirebaseFirestore
 
 class QT_TeacherQuiz : AppCompatActivity() {
@@ -23,7 +26,13 @@ class QT_TeacherQuiz : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_qt_teacher_quiz)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         firestore = FirebaseFirestore.getInstance()
         quizListContainer = findViewById(R.id.quiz_list_container)
